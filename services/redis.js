f71a1b53eabbe32f0f1340dcfe7158453c0a7497
stahsin00 <lamisa1998@hotmail.com>
2024-08-02 00:54:28 -0700

    containerize

--- services/redis.js
+++ services/redis.js
@@ -1,6 +1,11 @@
 import { createClient } from "redis";
 
-const client = createClient();
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const redisPort = process.env.REDIS_PORT || 6379;
+
+const client = createClient({
+    url: `redis://${redisHost}:${redisPort}`
+});
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
